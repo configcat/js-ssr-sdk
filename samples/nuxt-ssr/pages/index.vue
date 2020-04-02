bf706ce499e6e0ec16d1a5f418306356857b9e78
sige <sige@configcat.com>
2020-04-02 21:34:11 +0200

    nuxt sample

--- samples/nuxt-ssr/pages/index.vue
+++ samples/nuxt-ssr/pages/index.vue
@@ -2,40 +2,53 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        nuxt-ssr
-      </h1>
-      <h2 class="subtitle">
-        ConfigCat feature flags sample for NuxtJS
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <h2 class="subtitle">ConfigCat NuxtJs Sample App</h2>
+      <div>
+        <button v-on:click="checkAwesome" class="button--green">Check Awesome Feature</button>
+        <p>{{isAwesomeFeatureEnabled}}</p>
+      </div>
+      <div>
+        <button v-on:click="checkPOC" class="button--grey">Check POC Feature</button>
+        <p>{{isPOCFeatureEnabled}}</p>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<script lang="ts">
+import Vue from "vue";
+import Logo from "~/components/Logo.vue";
+import * as ConfiCat from "configcat-js-ssr";
 
-export default {
+const client = ConfiCat.createClientWithAutoPoll(
+  "7ZTVCKnUJprikI6Rwlj0RA/eJ8H21HZA06fDJrnzWyvGA",
+  { pollIntervalSeconds: 2 }
+);
+const value = false;
+
+export default Vue.extend({
   components: {
     Logo
+  },
+  data: () => {
+    return {
+      isAwesomeFeatureEnabled: false,
+      isPOCFeatureEnabled: false
+    };
+  },
+  methods: {
+    checkAwesome () {
+      client.getValue("isAwesomeFeatureEnabled", false, value => {
+        this.isAwesomeFeatureEnabled = value
+      });
+    },
+    checkPOC () {
+      client.getValue("isPOCFeatureEnabled", false, value => {
+        this.isPOCFeatureEnabled = value
+      });
+    }
   }
-}
+});
 </script>
 
 <style>
@@ -46,16 +59,6 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
 }
 
 .subtitle {
