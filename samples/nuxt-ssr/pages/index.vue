99489e8ceadd04af0038b309a2218f7dcc3ae658
sige <sige@configcat.com>
2020-04-07 20:44:04 +0200

    updating sample app

--- samples/nuxt-ssr/pages/index.vue
+++ samples/nuxt-ssr/pages/index.vue
@@ -7,7 +7,9 @@
         <button v-on:click="checkAwesome" class="button--green">Check Awesome Feature</button>
         <p>{{isAwesomeFeatureEnabled}}</p>
       </div>
-      <div>
+      <div class="poc">
+        <label for="user">Sample user:</label>
+        <input type="text" ref="user" name="user" v-model="user">
         <button v-on:click="checkPOC" class="button--grey">Check POC Feature</button>
         <p>{{isPOCFeatureEnabled}}</p>
       </div>
@@ -19,6 +21,7 @@
 import Vue from "vue";
 import Logo from "~/components/Logo.vue";
 import * as ConfiCat from "configcat-js-ssr";
+import { log } from "util";
 
 // Setting log level to 3 (= Info) to show detailed feature flag evaluation
 const logger = ConfiCat.createConsoleLogger(3);
@@ -38,7 +41,8 @@ export default Vue.extend({
   data: () => {
     return {
       isAwesomeFeatureEnabled: false,
-      isPOCFeatureEnabled: false
+      isPOCFeatureEnabled: false,
+      user: 'configcat@example.com'
     };
   },
   methods: {
@@ -51,8 +55,8 @@ export default Vue.extend({
       // Read more about the User Object: https://configcat.com/docs/sdk-reference/node/#user-object
       const userObject = {
         identifier: "#SOME-USER-ID#",
-        email: "configcat@example.com"
-      };
+        email: this.user
+      };      
       client.getValue(
         "isPOCFeatureEnabled",
         false,
@@ -74,6 +78,17 @@ export default Vue.extend({
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+
+.poc {
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.poc input {
+  margin-bottom: .5rem;
 }
 
 .subtitle {
